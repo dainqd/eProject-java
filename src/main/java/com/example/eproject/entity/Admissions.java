8697125a315b4d31b334e763d778f65c8ba7f752
bapdai <97323194+bapdai@users.noreply.github.com>
2022-11-01 18:26:36 +0700

    Create code course

--- src/main/java/com/example/eproject/entity/Admissions.java
+++ src/main/java/com/example/eproject/entity/Admissions.java
@@ -1,4 +1,18 @@
 package com.example.eproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Entity
+//@Table(name = "a")
 public class Admissions {
 }
--- src/main/java/com/example/eproject/entity/Course.java
+++ src/main/java/com/example/eproject/entity/Course.java
@@ -1,4 +1,44 @@
 package com.example.eproject.entity;
 
-public class Course {
+import com.example.eproject.entity.basic.BasicEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Currency;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "course")
+public class Course extends BasicEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    //    Mục tiêu
+    private String intent;
+    //    Điều kiện
+    private String condition;
+    //    Nội dung khóa học
+    private String content;
+    //    Bình luận
+    private String comments;
+    //    Nhận xét
+    private String reviews;
+    //    Giáo viên
+    private String trainer;
+    //    Học phí
+    private Currency price;
+    //    Chỗ ngồi
+    private int seat;
+    //    Thời gian bắt đầu
+    private Date startDate;
+    //    Thời gian Kết thúc
+    private Date endDate;
 }
