3ccc4054ea391bd311ef6f392aaf13a4f2ebbb05
unknown <ngodaix5tp@gmail.com>
2023-02-02 16:31:43 +0700

    Fix register + login restapi, delete mod api.

--- src/main/java/com/example/eproject/service/RoleService.java
+++ src/main/java/com/example/eproject/service/RoleService.java
@@ -2,13 +2,20 @@ package com.example.eproject.service;
 
 import com.example.eproject.entity.Role;
 import com.example.eproject.repository.RoleRepository;
+import com.example.eproject.util.Enums;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RoleService {
     @Autowired
     RoleRepository roleRepository;
+
+    public Optional<Role> findByName(Enums.Role name) {
+        return roleRepository.findByName(name);
+    }
 
     public Role save(Role srole) {
         return roleRepository.save(srole);
